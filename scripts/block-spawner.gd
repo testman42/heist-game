7fa847b32bbd164e050b876129e9e0887d3e9403
Martin Jakubik <martin@jakubik.info>
2022-04-02 14:10:57 +0200

    move files around for better structure

--- scripts/block-spawner.gd
+++ scripts/block-spawner.gd
@@ -2,6 +2,7 @@ extends Node
 class_name BlockSpawner
 
 # Spawns level blocks as the player is moving forward.
+# All blocks *must* be 100x100 meters.
 
 # path to the first block which must already be in the level
 export(NodePath) var firstBlock
