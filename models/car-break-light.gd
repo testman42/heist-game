befc330f836eab6cc6a5cc0b79071c0b3ed6c122
Martin Jakubik <martin@jakubik.info>
2022-04-03 01:26:02 +0200

    fix breaking lights

--- models/car-break-light.gd
+++ models/car-break-light.gd
@@ -1,19 +1,23 @@
 extends CSGBox
 
 onready var car := get_node('../../../..') as Car
+var previousSpeed = 0
 
 func _ready():
-	assert(car != null, 'Cannot find Car in its breaking lights')
-	material = material.duplicate()
+    assert(car, 'Cannot find Car in its breaking lights')
+    assert($bulb, 'Missing car breaking bulb')
+    material = material.duplicate()
 
 func _process(_delta):
-    if not $bulb:
-        return
+    var speed = abs(car.speed)
 
-    if abs(car.speed) < abs(car.previousSpeed) - .02 or abs(car.speed) < .01:
+    if speed < previousSpeed - .02 or speed < .1:
         $bulb.visible = true
         material.emission_energy = 2
     else:
         $bulb.visible = false
         material.emission_energy = .2
+
+
+    previousSpeed = speed
 
