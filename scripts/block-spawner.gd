f3c274aa6c0ef13057374dfdcffa2c57817e35e9
Martin Jakubik <martin@jakubik.info>
2022-04-02 13:59:06 +0200

    start with block spawner

---
+++ scripts/block-spawner.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name BlockSpawner
+
+# Spawns level blocks as the player is moving forward.
+
+# path to the first block which must already be in the level
+export(NodePath) var firstBlock
+
+
+
