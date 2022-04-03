42e88a67241b563506febad967c51c3271ae7b8e
Martin Jakubik <martin@jakubik.info>
2022-04-03 11:15:13 +0200

    smoother camera movement

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,15 +2,23 @@ extends Spatial
 
 onready var player := get_tree().get_nodes_in_group('player')[0] as Player
 
+func _ready():
+    # $tween.follow_method(self, 'getTarget', transform.origin, self, 'setPos', 10)
+    pass
+
 func _process(delta):
+    var target := getTarget()
+
+    target.y = move_toward(transform.origin.y, target.y, delta * 2.4 * min(3, abs(target.y - transform.origin.y)))
+    setPos(target)
+    $camera.look_at(target, Vector3.UP)
+
+func getTarget() -> Vector3:
     var target := player.get_transform().origin
+    target.x /= 2
+    target.y = player.speed / player.maxSpeed * 13
+    return target
 
-    var dolly := Vector3(0, get_transform().origin.y, target.z)
-    var targetHeight = player.speed / player.maxSpeed * 10
-
-    var newHeight = move_toward(dolly.y, targetHeight, 5 * delta)
-    dolly.y = newHeight
-
-    transform.origin = dolly
-    $camera.look_at(Vector3(0, 0, target.z), Vector3.UP)
+func setPos(pos: Vector3):
+    transform.origin = pos
 
