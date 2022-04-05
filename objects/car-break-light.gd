63c35429978d4b233ee0df396257e27125239cfd
Martin Jakubik <martin@jakubik.info>
2022-04-05 08:45:56 +0200

    rename light to bulb for consistency

--- objects/car-break-light.gd
+++ objects/car-break-light.gd
@@ -5,19 +5,19 @@ func _ready():
     if not GameController.lights:
         call_deferred('set_script', null)
 
-        if $light:
-            $light.queue_free()
+        if $bulb:
+            $bulb.queue_free()
     else:
         material = material.duplicate()
         material.emission_energy = 1
 
 func _on_car_startBreaking():
     material.emission_energy = 4
-    if $light:
-        $light.visible = true
+    if $bulb:
+        $bulb.visible = true
 
 
 func _on_car_stopBreaking():
     material.emission_energy = 1.5
-    if $light:
-        $light.visible = false
+    if $bulb:
+        $bulb.visible = false
