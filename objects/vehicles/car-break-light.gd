055c9081e9684995b7c31fc7e6c4be7a5a603cc2
Martin Jakubik <martin@jakubik.info>
2022-04-09 16:17:29 +0200

    handle correctly the case when breaking light has no bulb

--- objects/vehicles/car-break-light.gd
+++ objects/vehicles/car-break-light.gd
@@ -1,23 +1,24 @@
 extends CSGBox
 
+onready var bulb = get_node_or_null('bulb')
 
 func _ready():
     if not GameController.lights:
         call_deferred('set_script', null)
 
-        if $bulb:
-            $bulb.queue_free()
+        if bulb:
+            bulb.queue_free()
     else:
         material = material.duplicate()
         material.emission_energy = 1
 
 func _on_car_startBreaking():
     material.emission_energy = 4
-    if $bulb:
-        $bulb.visible = true
+    if bulb:
+        bulb.visible = true
 
 
 func _on_car_stopBreaking():
     material.emission_energy = 1.5
-    if $bulb:
-        $bulb.visible = false
+    if bulb:
+        bulb.visible = false
