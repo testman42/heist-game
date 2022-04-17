6beeca97c0f0e162855657cd0c9873e04bbd6f25
Martin Jakubik <martin@jakubik.info>
2022-04-17 11:25:33 +0200

    redo the main menu scene, add garage scene

--- ui/main-menu/main-buttons.gd
+++ ui/main-menu/main-buttons.gd
@@ -3,16 +3,16 @@ extends Control
 signal switchScene(name)
 
 
+func _on_garage_pressed():
+    emit_signal('switchScene', 'garage')
+
+
 func _on_quickplay_pressed():
     emit_signal('switchScene', 'quickplay')
 
 
 func _on_settings_pressed():
     emit_signal('switchScene', 'settings')
-
-
-func _on_controls_pressed():
-    emit_signal('switchScene', 'controls')
 
 
 func _on_exit_pressed():
