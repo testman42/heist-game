b8fb196a778d0ca2debd5a9694da08d7d5438f9b
Martin Jakubik <martin@jakubik.info>
2023-01-15 11:10:46 +0100

    add combi and sedan versions of cars

--- constants/car-constants.gd
+++ constants/car-constants.gd
@@ -5,17 +5,17 @@ class_name CarConstants
 const chanceToChangeLane = .0012
 const laneMatchThreshold = .3
 
-const spinningSpeedClamp = 10.
+const spinningSpeedClamp = 10.0
 const spinningRotation = .6
-const steeringRotation = 1/30.0
-const steeringSpeedAdjust = 1/20.0
+const steeringRotation = 1.0/30.0
+const steeringSpeedAdjust = 1.0/20.0
 
 const collisionSpeedMultiplier = .9
 const collisionSteeringMultiplier = .9
 const collisionHealthSpeedMultipler = .8
 const collisionHealthSteeringMultipler = .6
 
-const collisionSpinningThreshold = 16.
+const collisionSpinningThreshold = 16.0
 const healthSpinningThreshold = .2
 
 # Traffic spawning constants.
