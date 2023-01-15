aa8656e707bb115764183dacff56a86bf88bf712
Martin Jakubik <martin@jakubik.info>
2023-01-15 14:39:43 +0100

    fix kinematic objects getting deleted before played encounters them

--- utilities/dynamic-object.gd
+++ utilities/dynamic-object.gd
@@ -6,15 +6,21 @@ class_name DynamicObject
 # the block gets deleted, rather when they get too far behind the player.
 
 func _ready():
+    var targetTransform = global_transform
+
     # move the node to the root
     get_parent().call_deferred('remove_child', self)
     get_node('/root').call_deferred('add_child', self)
+
+    # reposition
+    call_deferred('set_transform', targetTransform)
+
 
 func _process(_delta):
     # delete if too far from the player
     var players := get_tree().get_nodes_in_group('player')
     if players.size() <= 0: return
 
-    if global_transform.origin.distance_squared_to(players[0].global_transform.origin) > HighwayConstants.blockLength * HighwayConstants.blockLength * 4:
+    if global_position.z > players[0].global_position.z + HighwayConstants.blockLength * 2:
         queue_free()
 
