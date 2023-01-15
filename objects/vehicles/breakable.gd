cea43b8f85836165ec1c8904f033457f569b9c4d
Martin Jakubik <martin@jakubik.info>
2023-01-15 21:04:28 +0100

    implement breakable bumpers

---
+++ objects/vehicles/breakable.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+# Breakable car part. When broken, it detaches itself from
+# the car and becomes a separate physics object.
+
+func breakOff():
+
+    var newNode := DynamicObject.new()
+
+    newNode.collision_layer = 0
+    newNode.set_collision_layer_value(7, true)
+
+    newNode.collision_mask = 0
+    for i in range(1, 8):
+        newNode.set_collision_mask_value(i, true)
+
+    # position the new object
+    newNode.transform = global_transform
+
+    # add force according to the current movement, and a random rotation
+    # TODO: newNode.apply_impulse(Vector3(steering, 0, speed * -heading))
+
+    # TODO: newNode.apply_torque_impulse(Vector3(
+    #     0,
+    #     clamp(speed, -CarConstants.spinningSpeedClamp, CarConstants.spinningSpeedClamp) * CarConstants.spinningRotation * spinningDirection,
+    #     0
+    # ))
+
+    # move child nodes over
+    for child in get_children():
+        remove_child(child)
+        newNode.add_child(child)
+
+    get_tree().root.add_child(newNode)
+    queue_free()
